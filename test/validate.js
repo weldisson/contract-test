99fc30ca15d4de7c129e1772957e9506ab58098f
Weldisson Araujo <weldissonaraujo@MBPdeWeldisson.lan>
2019-03-18 00:32:03 -0300

    alterando script de teste

--- test/validate.js
+++ test/validate.js
@@ -6,19 +6,21 @@ const correios = require('./schema');
 // link from request: http://www.mocky.io/v2/5c8822db320000af133bd6b6
 
 describe('Teste de contrato', () => {
-    it('GET', () => {
+    it('GET', done => {
         request('http://www.mocky.io')
         .get('/v2/5c8822db320000af133bd6b6')
         .expect('Content-type', 'application/json')
         .end((err, res) => {
-            if (err) return err;
+            if (err) return done(err);
+            
             let actual = res.body; 
             
             err = Joi.validate(actual, correios.schemaS(),
                     { abortEarly: false},
                 ).error
-            if (err) return err;
+            
             assert.equal(err, null);
+	    done();
         });
     });
 });
